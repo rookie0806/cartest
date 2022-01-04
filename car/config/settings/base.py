7aeabf453f7578e0037aa91944f816fd20c197b0
unknown <jbshs2013@naver.com>
2022-01-04 21:06:58 +0900

    test

--- car/config/settings/base.py
+++ car/config/settings/base.py
@@ -156,7 +156,6 @@ STATIC_ROOT = str(ROOT_DIR / "staticfiles")
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
-    str(APPS_DIR.path('static')),
     str(ROOT_DIR.path('frontend', 'build', 'static'))
 ]
 #STATICFILES_DIRS = [str(APPS_DIR / "static")]
